0c66338c7a9d2ad94b096713a46c934043fc2b71
Anushree <anushreepaul9699@gmail.com>
2022-07-26 12:07:05 +0530

    Printing Rectangle

--- SolidRectanglePrint.java
+++ SolidRectanglePrint.java
@@ -2,11 +2,12 @@ import java.lang.*;
 public class SolidRectanglePrint {
     public static void main(String[] args)
     {
-        int rows = 5 ;
+        int rows = 4 ;
+        int columns = 8 ;
         System.out.println("Printing Rectangle :");
         for(int i = 1 ; i<=rows ; i++)
         {
-            for(int j = 1 ; j <=rows ; j++)
+            for(int j = 1 ; j <=columns ; j++)
             {
                 System.out.print("*");
             }
