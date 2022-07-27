2705380f30f5479d6ae58e2dfea7a02dcc554cca
Anushree <anushreepaul9699@gmail.com>
2022-07-27 11:26:13 +0530

    0-1's Triangle

---
+++ ZeroOneTriangle.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+public class ZeroOneTriangle {
+    public static void main (String[] args)
+    {
+        int r = 5 ;
+        for(int i = 1 ; i<=r ; i++)
+        {
+            for(int j = 1 ; j<=i ; j++)
+            {
+                if((i+j)%2==0)
+                {
+                    System.out.print("1"+" ");
+                }
+                else
+                {
+                    System.out.print("0"+" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
